7382a76e11d387ac7d297aa0a9acdcaf4be9f8ec
mblogger <mohitblogger@gmail.com>
2020-10-26 19:23:46 +0530

    BW-1 Initial setup and web3js integration with smart contracts

--- app.js
+++ app.js
@@ -2,29 +2,55 @@ const Web3 = require('web3');
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
 
 async function getBlockNumber() {
-    const latestBlockNumber = await web3.eth.getBlockNumber();
-    console.log("Block Number: " + latestBlockNumber);
+    try {
+        const latestBlockNumber = await web3.eth.getBlockNumber();
+        console.log("Block Number: " + latestBlockNumber);
+    } catch (error) {
+        console.log('Unable to connect with the local RPC!')        
+    }
 }
 
 getBlockNumber();
 
 async function sendMoney() {
+    var fs = require('fs');
     // set the default address
     web3.eth.defaultAccount = web3.eth.accounts[0];
-    var fs = require('fs');
     var jsonObj;
-    fs.readFile('block3web/build/contracts/MoneyTransfer.json', 'utf8', function (err, data) {
-        if (err) {
-            throw err;
-        }
-        jsonObj = JSON.parse(data);
-        if (jsonObj) {
-            const moneyTransfer = new web3.eth.Contract(jsonObj.abi);
-            moneyTransfer.sendMoney(web3.eth.accounts[0], web3.eth.accounts[1], 10000000, 100);
-        } else {
-            console.log("Couldn't read the contract JSON!!");
+    var accessible = false;
+    var scMoneyTransfer = __dirname + '/build/contracts/MoneyTransfer.json';
+
+    // Check access to the file
+    fs.access(scMoneyTransfer, fs.constants.F_OK, (err) => {
+        accessible = err ? false : true;
+        console.log(`${scMoneyTransfer} ${err ? 'does not exists': 'exists'}`);
+        if (accessible) {
+            // Read ABI from smart contract
+            try {
+                fs.readFile(scMoneyTransfer, 'utf8', function (err, data) {
+                    jsonObj = JSON.parse(data);
+                    if (jsonObj) {
+                        const moneyTransfer = new web3.eth.Contract(jsonObj.abi);
+                        moneyTransfer.methods
+                            .sendMoney(web3.eth.accounts[0], web3.eth.accounts[1], 10000000, 100)
+                            .call(function(err, res) {
+                                if (err) {
+                                    console.log('Error occured while sending money: ', err);
+                                    return;
+                                }
+                                console.log('Result of money sent:', res);
+                            });
+                    } else {
+                        console.log(`${scMoneyTransfer} could not parse. Technical issue!`);
+                    }
+                });
+            } catch (err) {
+                console.log(`Error while reading ${scMoneyTransfer}. Aborting! Error message: ${error.message}`);
+                return;
+            }
         }
     });
+
 }
 
 sendMoney();
